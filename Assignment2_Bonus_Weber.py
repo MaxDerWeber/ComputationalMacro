36e493f5cf17228c5d5adb3c49feba853568c1f5
Maximilian Weber <webermaximilian1007@gmail.com>
2021-12-18 14:00:15 +0100

    Added solution from prof.

--- Assignment2_Bonus_Weber.py
+++ Assignment2_Bonus_Weber.py
@@ -18,6 +18,9 @@ A_inv = np.linalg.inv(A)
 #1
 
 def shock_model1(expec_vec):
+    phi_1=1.5
+    phi_2=0.2
+
     B_shocked = np.zeros((3,2))
     B_ex = np.zeros((3,2)) # expected values
 
@@ -25,10 +28,9 @@ def shock_model1(expec_vec):
     expPi = expec_vec[1]
     expi = expec_vec[2]
 
-
     e1 = expec_vec[3]
     e2 = expec_vec[4]
-    e3 = expec_vec[5] #in c) it is 1
+    e3 = expec_vec[5] #in 3) it is 1
 
     # B as 3x2 matrix instead of 3x1
     # expected values, shocks with mean 0
@@ -55,3 +57,112 @@ print(coefficients,msg)
 init3 = [1,0,1,1,0,1]
 [coefficients3, inf, ier, msg3] = fsolve(shock_model1, init, full_output = True)
 print(coefficients3,msg3)
+
+
+# takeaways from the solution:
+    # The input needs to be effect from static variable and effect from the shocks, not the shocks themselves
+    # Also it is said, that in 3. the input vector has length 9, which is not the case in my solution
+    # the output then needs to be the three effect-size variables from the model with [0:3] and without [3:6] the shocks
+
+
+
+
+#### Solution from the lecture:
+
+# 1.
+def statefunc(coef):
+    phi1=1.5
+    phi2=0.2
+
+    C_outcome = np.zeros(len(coef))
+    c0 = C_outcome[0:3]
+    c1 = C_outcome[3:6] # not used in 1)
+
+    # The constants and the shocks both show in the msv
+    B = np.zeros(3)
+
+    # First we calculate the contribution of the constants to the model outcome
+    expY = c0[0]
+    exPi = c0[1]
+    shockX = 0
+
+
+    B[0] = expY + 1/sigma * exPi + shockX
+    B[1] = beta * exPi
+    B[2] = phi1 * exPi + phi2 * expY
+
+    C_outcome[0:3] = np.dot(A_inv,B)
+
+    # Next, we calculate the contribution of the shock to the model:
+    B = np.zeros(3)
+    expY = c0[0]
+    exPi = c0[1]
+
+    # the interpretation of C1 is the effect of a shock of size 1 on z_t
+    shockX = 1
+
+    B[0] = expY + 1/sigma * exPi + shockX
+    B[1] = beta * exPi
+    B[2] = phi1 * exPi + phi2 * expY
+
+    C_outcome[3:6] = np.dot(A_inv, B) - C_outcome[0:3]
+    equations = C_outcome - np.array(coef)
+    return equations
+
+# 2.
+init = [0] * 6
+[msv_coefficients, inf, ier, msg] = fsolve(statefunc, init, full_output = True)
+if ier != 0:
+    print(msg)
+
+msv_constants = msv_coefficients[0:3]
+msv_shock_Y_coefs = msv_coefficients[3:6]
+
+print('The constants in the msv solution are: ', msv_constants)
+print('The coefficients on the output gap shock are: ', msv_shock_Y_coefs)
+
+# 3.
+pos = 0
+
+def statefunc(coef):
+    C_outcome = np.zeros(len(coef))
+    c0 = C_outcome[0:3]
+    c1 = C_outcome[3:6]
+    c2 = C_outcome[6:9]
+
+    phi1=1.5
+    phi2=0.2
+
+    for [shock_Y, shock_i] in [[0,0], [1,0], [0,1]]:
+        expY = c0[0]
+        exPi = c0[1]
+
+        B = np.zeros(3) # initialize for each iteration
+
+        # r_bar, X_target, Pi_target are constant and should aonly be considered when looking at constants (for later???)
+        B[0] = expY + 1/sigma * exPi + shock_Y
+        B[1] = beta * exPi
+        B[2] = phi1 * exPi + phi2 * expY + shock_i
+
+        if pos == 0:
+            C_outcome[pos:pos+3] = np.dot(A_inv,B)
+        else:
+            C_outcome[pos:pos+3] = np.dot(A_inv,B)-C_outcome[0:3]
+        
+    equations = C_outcome - np.array(coef)
+    return equations
+
+init = [0] * 9
+
+[msv_coefficients, inf, ier, msg] = fsolve(statefunc, init, full_output = True)
+if ier != 0:
+    print(msg)
+
+msv_constants = msv_coefficients[0:3]
+msv_shock_Y_coefs = msv_coefficients[3:6]
+msv_shock_Pi_coefs = msv_coefficients[6:9]
+
+
+print('The constants in the msv solution are: ', msv_constants)
+print('The coefficients on the output gap shock are: ', msv_shock_Y_coefs)
+print('The coefficients on the interest rate shock are: ', msv_shock_Y_coefs)
